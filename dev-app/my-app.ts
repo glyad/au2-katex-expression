aef470157a5bcf8ab1cd4314b5cb98c09224ec4b
David Kossoglyad (Hire me!) <david@logoui.co.il>
2024-01-08 12:29:05 +0200

    feat: improve documentation and demo

--- dev-app/my-app.ts
+++ dev-app/my-app.ts
@@ -1,6 +1,33 @@
+
 export class MyApp {
 
   public expression = 'f(x) = \\int_{-\\infty}^\\infty \\hat f(\\xi)\\,e^{2 \\pi i \\xi x} \\,d\\xi';
+
+  public displayMode = true;
+
+  public output = 'html';
+
+  public throwOnError: boolean = true;
+
+  public errorColor: string = '#cc0000';
+
+  public leqno: boolean = false;
+
+  public fleqn: boolean = false;
+
+  public minRuleThickness = 0.04;
+
+  public colorIsTextColor: boolean = false;
+
+  public maxSize: number = 5;
+
+  public maxExpand = 1000;
+
+  public strict = 'warn';
+
+  public trust: boolean = false;
+
+  public globalGroup: boolean = false;
 
   public tex = `Systems of equations naturally appear in linear algebra through vector equations.`
 }
--- src/katex-expression.ts
+++ src/katex-expression.ts
@@ -4,9 +4,11 @@ import { bindable } from 'aurelia';
 import { watch } from '@aurelia/runtime-html';
 import katex from 'katex';
 
+type Output = "html" | "mathml" | "htmlAndMathml";
+
 export interface IKatexOptions {
     displayMode?: boolean;
-    output?: "html" | "mathml" | "htmlAndMathml";
+    output?: Output;
     leqno?: boolean;
     fleqn?: boolean;
     throwOnError?: boolean;
@@ -47,15 +49,34 @@ export class KatexExpression {
 
   @bindable public expression: string;
 
+  
   @bindable
+  /**
+   * Render math in display mode, which puts the math in " +
+            "display style (so \\int and \\sum are large, for example), and " +
+            "centers the math on the page on its own line.
+   * 
+   * @returns {boolean} The display mode value.
+   */
   public get displayMode(): boolean {
     return this._options.displayMode;
   }
-  public set displayMode(value: boolean) {
-    this._options.displayMode = value;
+  public set displayMode(value: boolean | string) {
+    if (value === "true") {
+      value = true;
+    } else if (value === "false") {
+      value = false;
+    }
+    this._options.displayMode = <boolean>value;
   }
 
   @bindable
+  /**
+   * Gets the output value. 
+   * Determines the markup language of the output.
+   * 
+   * @returns The output value.
+   */
   public get output() {
     return this._options.output;
   }
@@ -64,6 +85,12 @@ export class KatexExpression {
   }
 
   @bindable
+  /**
+   * Gets the value of the leqno property.
+   * Render display math in leqno style (left-justified tags).
+   * 
+   * @returns The value of the leqno property.
+   */
   public get leqno() {
     return this._options.leqno;
   }
@@ -72,6 +99,12 @@ export class KatexExpression {
   }
 
   @bindable
+  /**
+   * Gets the value of the fleqn option.
+   * Render display math flush left.
+   * 
+   * @returns The value of the fleqn option.
+   */
   public get fleqn() {
     return this._options.fleqn;
   }
@@ -80,6 +113,13 @@ export class KatexExpression {
   }
 
   @bindable
+  /**
+   * Gets the value of the throwOnError option.
+   * Render errors (in the color given by --error-color) instead of 
+   * throwing a ParseError exception when encountering an error.
+   * 
+   * @returns {boolean} The value of the throwOnError option.
+   */
   public get throwOnError() {
     return this._options.throwOnError;
   }
@@ -88,6 +128,12 @@ export class KatexExpression {
   }
 
   @bindable
+  /**
+   * Gets the error color.
+   * A color string given in the format 'rgb' or 'rrggbb' (no #).
+   * 
+   * @returns The error color.
+   */
   public get errorColor() {
     return this._options.errorColor;
   }
@@ -96,6 +142,12 @@ export class KatexExpression {
   }
 
   @bindable
+  /**
+   * Gets the macros for the Katex expression.
+   * Define custom macro of the form '\\foo:expansion'.
+   * 
+   * @returns The macros object.
+   */
   public get macros() {
     return this._options.macros;
   }
@@ -104,6 +156,14 @@ export class KatexExpression {
   }
 
   @bindable
+  /**
+   * Gets the minimum rule thickness.
+   * Specifies a minimum thickness, in ems, for fraction lines, `\\sqrt` top lines, 
+   * `{array}` vertical lines, `\\hline`, `\\hdashline`, `\\underline`, `\\overline`, 
+   * and the borders of `\\fbox`, `\\boxed`, and `\\fcolorbox`.
+   * 
+   * @returns The minimum rule thickness.
+   */
   public get minRuleThickness() {
     return this._options.minRuleThickness;
   }
@@ -112,6 +172,11 @@ export class KatexExpression {
   }
 
   @bindable
+  /**
+   * Gets the value indicating whether the color is used as the text color.
+   * 
+   * @returns {boolean} The value indicating whether the color is used as the text color.
+   */
   public get colorIsTextColor() {
     return this._options.colorIsTextColor;
   }
@@ -120,6 +185,10 @@ export class KatexExpression {
   }
 
   @bindable
+  /**
+   * Gets the maximum size of the expression.
+   * @returns The maximum size.
+   */
   public get maxSize() {
     return this._options.maxSize;
   }
@@ -128,6 +197,14 @@ export class KatexExpression {
   }
 
   @bindable
+  /**
+   * Gets the maximum expansion value.
+   * Limit the number of macro expansions to the specified number, to prevent 
+   * e.g. infinite macro loops. If set to Infinity, the macro expander 
+   * will try to fully expand as in LaTeX.
+   * 
+   * @returns The maximum expansion value.
+   */
   public get maxExpand() {
     return this._options.maxExpand;
   }
@@ -136,6 +213,13 @@ export class KatexExpression {
   }
 
   @bindable
+  /**
+   * Gets the value of the strict option.
+   * Turn on strict / LaTeX faithfulness mode, which throws an error, 
+   * if the input uses features that are not supported by LaTeX.
+   * 
+   * @returns {boolean} The value of the strict option.
+   */
   public get strict() {
     return this._options.strict;
   }
@@ -144,6 +228,12 @@ export class KatexExpression {
   }
 
   @bindable
+  /**
+   * Gets the trust level of the expression.
+   * Trust the input, enabling all HTML features such as \\url.
+   * 
+   * @returns The trust level.
+   */
   public get trust() {
     return this._options.trust;
   }
@@ -152,6 +242,13 @@ export class KatexExpression {
   }
 
   @bindable
+  /**
+   * Gets the global group value. If non-zero, all user-specified sizes, e.g. in 
+   * \\rule{500em}{500em}, will be capped to maxSize ems. Otherwise, elements and 
+   * spaces can be arbitrarily large.
+   * 
+   * @returns The global group value.
+   */
   public get globalGroup() {
     return this._options.globalGroup;
   }
